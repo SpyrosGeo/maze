81f42467a1382b3ed36d2431c873d529eb661ee9
SpyrosGeo <xarodasimba@gmail.com>
2020-04-22 17:29:50 +0300

    refactored some code for more flexibility down the line

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
-const {Engine,Render,Runner,World,Bodies} = Matter;
+const {Engine,Render,Runner,World,Bodies, } = Matter;
 
 
 const engine = Engine.create();
 const {world} = engine
-
+const width =600;
+const height=600;
 const render = Render.create({
     element:document.body,
     engine:engine,
     options:{
-        width:800,
-        height:600,
+        width,
+        height,
     }
 
 });
 
 Render.run(render);
 Runner.run(Runner.create(),engine);
+
+//walls
+const walls = [
+    Bodies.rectangle(width/2,0,width,40,{
+        isStatic:true
+    }), Bodies.rectangle(width/2, 600, width, 40, {
+        isStatic: true
+    }),
+    Bodies.rectangle(0,height/2,40,height,{
+        isStatic:true
+    }),
+    Bodies.rectangle(height,height/2,40,height,{
+        isStatic:true
+    }),
+
+]
+
+ 
+
+World.add(world,walls);
